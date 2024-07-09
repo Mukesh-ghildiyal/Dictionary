1df9a31df174275d468535389471dd8b88372e7d
Mukesh-ghildiyal <115055244+Mukesh-ghildiyal@users.noreply.github.com>
2024-07-09 17:29:58 +0530

    second commit

--- src/component/Foooter/Footer.jsx
+++ src/component/Foooter/Footer.jsx
@@ -6,19 +6,17 @@ const Footer = () => {
         <div className="footer">
             <hr style={{ width: "90%", marginTop: 20 }} />
             <span className="name">
-                Made by{" "}
-                <a href="https://www.youtube.com/c/RoadsideCoder" target="__blank">
-                    Roadside Coder
-                </a>
+                Made by{" "}      
+                    Mukesh Ghildiyal
             </span>
             <div className="iconContainer">
-                <a href="https://www.instagram.com/roadsidecoder/" target="__blank">
+                <a href="" target="__blank">
                     <i className="fab fa-instagram-square fa-2x"></i>
                 </a>
-                <a href="https://www.linkedin.com/in/piyush-eon" target="__blank">
+                <a href="" target="__blank">
                     <i className="fab fa-linkedin fa-2x"></i>
                 </a>
-                <a href="https://www.youtube.com/c/RoadsideCoder" target="__blank">
+                <a href="" target="__blank">
                     <i className="fab fa-youtube fa-2x"></i>
                 </a>
             </div>
